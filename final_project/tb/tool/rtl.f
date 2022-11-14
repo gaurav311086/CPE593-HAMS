d73ea9bd551c58845cd3fcb8df4629cb2a9f192e
gaurav311086 <gaurav311086@gmail.com>
2022-11-14 01:42:41 -0500

    Added support for Intel-Modelsim

---
+++ final_project/tb/tool/rtl.f
@@ -0,0 +1,5 @@
+../../rtl/systemverilog/hams_pkg.vh
+../../rtl/systemverilog/hams_pipevld.sv
+../../rtl/systemverilog/hams_syncfifo.sv
+../../rtl/systemverilog/hams_sort2elem.sv
+../../rtl/systemverilog/hams_sortNelem.sv
