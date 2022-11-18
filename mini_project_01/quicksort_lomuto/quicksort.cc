8cf87e1529777ea69bf49b3185a421c09b077f96
gaurav311086 <gaurav311086@gmail.com>
2022-11-18 02:12:32 -0500

    Fixed quicksort Hoare

--- mini_project_01/quicksort_lomuto/quicksort.cc
+++ mini_project_01/quicksort_lomuto/quicksort.cc
@@ -16,7 +16,10 @@ Hoare partition reference
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <random>
 using namespace std;
+random_device rd;
+mt19937 gen(rd());
 
 #define PATTERN_INCR  0
 #define PATTERN_DECR  1
@@ -31,113 +34,35 @@ void  exchange(int * arr, int a, int b);
 int   partition_hoare(int *array, int low, int high);
 void  quicksort_hoare(int *array, int low, int high, int k=0);
 int   lomuto_parition(int * a, int lo, int hi);
-void  quicksort_lomuto(int * a, int lo, int hi, int ins_sort_max_lvl=8);
+void  quicksort_lomuto(int * a, int lo, int hi, int ins_sort_max_lvl=0);
 void  insertionsort(int * a, int lo, int hi);
 void  fischeryates_shuffle(int * a, int lo, int hi);
 void  fill_array(int *a,uint32_t n, uint32_t pattern=PATTERN_RAND3);
 void  printHeadnTail(int *a,uint32_t size, uint32_t amount=10);
-void  test_quicksort_lomuto(int * a, uint32_t n, uint32_t k);
-void  test_quicksort_hoare(int * a, uint32_t n, uint32_t k);
-int   goldensearch(int *array, int size);
+void  test_quicksort_lomuto(int * a, uint32_t n, uint32_t experiments,  uint32_t k=0);
+void  test_quicksort_hoare(int * a, uint32_t n, uint32_t experiments, uint32_t k=0);
+int   goldensearch(const float *array, const int size);
+int   random(int low, int high);
+void  extra_credit_knuth_optimized_qsort_hoare(const int n);
+void  extra_credit_knuth_optimized_qsort_lomuto(const int n);
 
 int main() {
-  uint32_t n      = 100000000;
-  // uint32_t n      = 100;
-  int experiments = 15;
+  // uint32_t n      = 100000000;
+  uint32_t n      = 1000000;
   int patterns [4] = {PATTERN_INCR, PATTERN_DECR, PATTERN_REPP, PATTERN_INCR};
-  int k[15]       = {4, 8, 16, 32, 64 ,128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+  int experiments = 15;
+  // int k[15]       = {4, 8, 16, 32, 64 ,128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
   int *arr        = new int[n];
   
-  cout << "Testing Quicksort (Lomuto) " << endl;
-  for(int tst =0; tst < 4; tst++){
-    if(tst==0)
-    cout << "Test with array already sorted--ascending!"<<endl;
-    if(tst==1)
-    cout << "Test with array already sorted--descending!"<<endl;
-    if(tst==2)
-    cout << "Test with array--with some number repeated!"<<endl;
-    if(tst==3)
-    cout << "Test with array--with unique but random data!"<<endl;
-    for(int run = 0; run < experiments; run++){
-      fill_array(arr,n,patterns[tst]);
-      if(tst==3 || tst==2) fischeryates_shuffle(arr,0,n);
-      test_quicksort_lomuto(arr,n,k[run]);
-    }
-  }  
   
+  test_quicksort_hoare(arr,n,experiments);
+  test_quicksort_lomuto(arr,n,experiments);
   
-  
-  //Knuth Optimized quicksort
-  
-  // initializes variables
-  clock_t t1;
-  clock_t t2;
-  double t;
-  int size = 10000000; // sets the size of the array
-  // int size = 100; // sets the size of the array
-  int* array = new int[size];
-  int* origarray = new int[size];
-  int* k_array = new int[100];
-
-  // populate the array
-  for(int i = 0; i < size; i++)
-      array[i] = (i + 1)*(i + 1);
-  fischeryates_shuffle(array,0, size); // randomize the array
-  // save the shuffled array
-  for(int i = 0; i < size; i++){
-      origarray[i] = array[i];
-  }
-  // highest value of k to test - used 100 due to other tests showing values over 100 performed worse
-  int k_size = 100;
-
-  // run through different values of k
-  for(int k = 1; k <= k_size; k++){
-      // reset back to the shuffled array for each value of k
-      for(int i = 0; i < size; i++){
-          array[i] = origarray[i];
-      }
-      t1 = clock();
-      quicksort_lomuto(array, 0, size -1, k);
-      t2 = clock();
-      std::cout << "n=" << size << "   time=" << (t2-t1)<< "   k=" << k << std::endl;
-      // printit(array, size);
-      k_array[k-1] = (t2-t1); // save the values of runtime for each k
-  }
-
-  int k_min = goldensearch(k_array, k_size);
-  cout << endl << "best k value is " << k_min+1 << " with a time of " <<  k_array[k_min] << " machine cycles."<<endl;
-
-  delete[] array;
-  delete[] origarray;
-  delete[] k_array;
-  array=nullptr;
-  origarray=nullptr;
-  k_array=nullptr;
-  
+  extra_credit_knuth_optimized_qsort_hoare(n);
+  extra_credit_knuth_optimized_qsort_lomuto(n);
   
   delete [] arr;
-  arr=nullptr;
-  cout << "Testing Original Quicksort (Hoare) " << endl;
-  for(int tst =0; tst < 4; tst++){
-    arr        = new int[n];
-    if(tst==0)
-    cout << "Test with array already sorted--ascending!"<<endl;
-    if(tst==1)
-    cout << "Test with array already sorted--descending!"<<endl;
-    if(tst==2)
-    cout << "Test with array--with some number repeated!"<<endl;
-    if(tst==3)
-    cout << "Test with array--with unique but random data!"<<endl;
-    for(int run = 0; run < experiments; run++){
-      fill_array(arr,n,patterns[tst]);
-      if(tst==3 || tst==2) fischeryates_shuffle(arr,0,n);
-      test_quicksort_hoare(arr,n,k[run]);
-    }
-    delete [] arr;
-    arr=nullptr;
-  }
-  
-  
+  arr=nullptr; 
   return 0;
 }
 void  exchange(int * arr, int a, int b) {
@@ -148,8 +73,9 @@ void  exchange(int * arr, int a, int b) {
   arr[b]    = temp;
 }
 int lomuto_parition(int * a, int lo, int hi){
-  int random_i = abs((  int) random())%(hi-lo+1);
-  int rand_pivot = lo + random_i;
+  // int random_i = abs((  int) random(low,high))%(hi-lo+1);
+  int random_i = random(lo,hi);
+  int rand_pivot = lo + (random_i%(hi-lo+1));
   exchange(a,rand_pivot,hi);
   
   int pivot = hi;
@@ -170,7 +96,7 @@ void  quicksort_lomuto(int * a, int lo, int hi, int ins_sort_max_lvl){
     return;
   int start = lo;
   int end = hi;
-  if((end - start) >= ins_sort_max_lvl){
+  if((ins_sort_max_lvl <=0 ) || ((end - start) >= ins_sort_max_lvl)){
     // lomuto partition    
     int pivot = lomuto_parition(a,start,end);
     quicksort_lomuto(a,start,pivot-1);
@@ -183,8 +109,8 @@ void  quicksort_lomuto(int * a, int lo, int hi, int ins_sort_max_lvl){
 int partition_hoare(int *array, int low, int high){
   //pick a random index in array and swap it with low
   int len = (high-low+1);
-  int random_i = (abs((  int) random()))%len;
-  int rand_index = random_i + low;
+  int random_i = random(low,high);
+  int rand_index = (random_i%len) + low;
   exchange(array,rand_index,low);
 
   int pivot = array[low];
@@ -194,12 +120,12 @@ int partition_hoare(int *array, int low, int high){
     do {
       i++;
     }
-    while(array[i] < pivot);
+    while(array[i] < pivot && (i<high));
       
     do {
       j--;
     }
-    while(array[j] >= pivot);
+    while(array[j] >= pivot && (j>low));
       
     if(i >= j)
       return j;
@@ -210,15 +136,17 @@ int partition_hoare(int *array, int low, int high){
 
 // function to continually breaks down the arrays to perform the quicksort
 void quicksort_hoare(int* array, int low, int high, int k){
-    if(k>0 && ((high - low) < k))
-        insertionsort(array, low, high);
-    else{
-      if(low < high){
-          int part_index = partition_hoare(array, low, high);
-          quicksort_hoare(array, low, part_index-1,k);
-          quicksort_hoare(array, part_index + 1, high,k);
-      }
+  if (low >= high)
+    return;
+  if(k>0 && ((high - low) < k))
+      insertionsort(array, low, high);
+  else{
+    if(low < high){
+        int part_index = partition_hoare(array, low, high);
+        quicksort_hoare(array, low, part_index-1,k);
+        quicksort_hoare(array, part_index + 1, high,k);
     }
+  }
 }
 void  insertionsort(int * a, int lo, int hi){
   if (!(lo < hi))
@@ -239,45 +167,80 @@ void  insertionsort(int * a, int lo, int hi){
 void  fischeryates_shuffle(int * a, int lo, int hi){
   int len = hi - lo + 1;
   for(int i =len-1; i >= lo+1; i--){
-    int random_i = (abs((  int) random()))%(hi-lo+1);
-    int r = lo + random_i;
+    // int random_i = (abs((  int) random(low,high)))%(hi-lo+1);
+    int random_i = random(lo,hi);
+    int r = lo + (random_i%(hi-lo+1));
     exchange(a,r,i);
   }
 }
-void test_quicksort_lomuto(int *a, uint32_t n, uint32_t k){
-  clock_t start, end;
-  #ifdef DEBUG
-  cout<<"Unsorted array:";
-  printHeadnTail(a,n);
-  #endif
-  start = clock();
-  quicksort_lomuto(a,0,n-1,k);
-  end = clock();
-  #ifdef DEBUG
-  cout<<"Qsorted array:";
-  printHeadnTail(a,n);
-  #endif
-  double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  cout  << "n=" << n <<" time = " <<cpu_time_used << " seconds. k = " << k << endl;
+void test_quicksort_lomuto(int *a, uint32_t n, uint32_t experiments, uint32_t k){
+  int patterns [4] = {PATTERN_INCR, PATTERN_DECR, PATTERN_REPP, PATTERN_INCR};
+  cout <<endl <<  "Testing Quicksort (Lomuto) " <<endl <<  endl;
+  for(int tst =0; tst < 4; tst++){
+    if(tst==0)
+    cout << "Test with array already sorted--ascending!"<<endl;
+    if(tst==1)
+    cout << "Test with array already sorted--descending!"<<endl;
+    if(tst==2)
+    cout << "Test with array--with some number repeated!"<<endl;
+    if(tst==3)
+    cout << "Test with array--with unique but random data!"<<endl;
+    for(int run = 0; run < experiments; run++){
+      fill_array(a,n,patterns[tst]);
+      if(tst==3 || tst==2) fischeryates_shuffle(a,0,n-1);
+      clock_t start, end;
+      #ifdef DEBUG
+      cout<<"Unsorted array:";
+      printHeadnTail(a,n);
+      #endif
+      start = clock();
+      quicksort_lomuto(a,0,n-1,k);
+      end = clock();
+      #ifdef DEBUG
+      cout<<"Qsorted array:";
+      printHeadnTail(a,n);
+      #endif
+      double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      cout  << "n=" << n <<" time = " <<cpu_time_used << " seconds. k = " << k << endl;
+    }
+  }
 }
-void test_quicksort_hoare(int *a, uint32_t n, uint32_t k){
-  clock_t start, end;
-  #ifdef DEBUG
-  cout<<"Unsorted array:";
-  printHeadnTail(a,n);
-  #endif
-  start = clock();
-  quicksort_hoare(a,0,n-1,k);
-  end = clock();
-  #ifdef DEBUG
-  cout<<"Qsorted array:";
-  printHeadnTail(a,n);
-  #endif
-  double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  cout  << "n=" << n <<" time = " <<cpu_time_used << " seconds. k = " << k << endl;
+void test_quicksort_hoare(int *a, uint32_t n, uint32_t experiments, uint32_t k){
+  int patterns [4] = {PATTERN_INCR, PATTERN_DECR, PATTERN_REPP, PATTERN_INCR};
+  cout << endl << "Testing Original Quicksort (Hoare) " << endl << endl;
+  for(int tst =0; tst < 4; tst++){
+    if(tst==0)
+    cout << "Test with array already sorted--ascending!"<<endl;
+    if(tst==1)
+    cout << "Test with array already sorted--descending!"<<endl;
+    if(tst==2)
+    cout << "Test with array--with some number repeated!"<<endl;
+    if(tst==3)
+    cout << "Test with array--with unique but random data!"<<endl;
+    for(int run = 0; run < experiments; run++){
+      fill_array(a,n,patterns[tst]);
+      if(tst==3 || tst==2) fischeryates_shuffle(a,0,n-1);
+      clock_t start, end;
+      #ifdef DEBUG
+      cout<<"Unsorted array:";
+      printHeadnTail(a,n);
+      #endif
+      start = clock();
+      quicksort_hoare(a,0,n-1,k);
+      end = clock();
+      #ifdef DEBUG
+      cout<<"Qsorted array:";
+      printHeadnTail(a,n);
+      #endif
+      double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      cout  << "n=" << n <<" time = " <<cpu_time_used << " seconds. k = " << k << endl;
+    }
+  }
+
 }
 void fill_array(int *arr, uint32_t n, uint32_t pattern){
-  int random_i = abs((  int) random());
+  // int random_i = abs((  int) random(low,high));
+  int random_i = random(0,n-1);
   int rp_this = (n/16);
   for (uint32_t i = 0; i < n; i++) {
     switch(pattern){
@@ -291,15 +254,15 @@ void fill_array(int *arr, uint32_t n, uint32_t pattern){
         arr[i]=(i%rp_this == 0)? rp_this : i;
         break;
       case PATTERN_RAND:
-        random_i = abs((  int) random());
-        arr[i]=random_i%10000;
+        // random_i = abs((  int) random(low,high));
+        arr[i]=random(0,n-1);
         break;
       case PATTERN_RAND3:
-        random_i = abs((  int) random());
+        random_i = random(0,n-1);
         arr[i]=random_i%i;
         break;
       default:
-        random_i = abs((  int) random());
+        random_i = random(0,n-1);
         // arr[i]=distribution(generator)%i;
         arr[i]=random_i%i;
         break;
@@ -322,7 +285,7 @@ void  printHeadnTail(int *a,uint32_t size, uint32_t amount){
 }
 
 // golden mean search function to find the minimum
-int goldensearch(int *array, int size){
+int goldensearch(const float *array, const int size){
     int x1 = 0;
     int x4 = size - 1;
     double phi = 1.618;
@@ -347,4 +310,109 @@ int goldensearch(int *array, int size){
         return x3;
     else
         return x2;
+}
+int random(int low, int high){
+    uniform_int_distribution<> dist(low, high);
+    return dist(gen);
+}
+void extra_credit_knuth_optimized_qsort_hoare(const int n) {
+  cout << endl << "Knuth optimized qsort hoare " << endl << endl;
+  //Knuth Optimized quicksort
+  
+  // initializes variables
+  clock_t t1;
+  clock_t t2;
+  double t;
+  int size = n; // sets the size of the array
+  int* array = new int[size];
+  int* origarray = new int[size];
+  constexpr int k_size = 21;
+  float* k_array = new float[k_size];
+
+  // populate the array
+  for(int i = 0; i < size; i++)
+      array[i] = (i + 1)*(i + 1);
+  fischeryates_shuffle(array,0, size-1); // randomize the array
+  // save the shuffled array
+  for(int i = 0; i < size; i++){
+      origarray[i] = array[i];
+  }
+
+  // run through different values of k
+  int kparam;
+  for(int k = 0; k < k_size; k++){
+     kparam = (k > 0)?((k/10)*1000+k*100):((k/10)*1000+k*100+4);
+      // reset back to the shuffled array for each value of k
+      for(int i = 0; i < size; i++){
+          array[i] = origarray[i];
+      }
+      t1 = clock();
+      quicksort_hoare(array, 0, size -1, kparam);
+      t2 = clock();
+      std::cout << "n=" << size << "   time=" << ((double) (t2 - t1)) / CLOCKS_PER_SEC << "   k=" << kparam << endl;
+      // printit(array, size);
+      k_array[k] = ((double) (t2 - t1)) / CLOCKS_PER_SEC; // save the values of runtime for each k
+  }
+
+  int k_min = goldensearch(k_array, k_size);
+  kparam = (k_min > 0)?((k_min/10)*1000+k_min*100):((k_min/10)*1000+k_min*100+4);
+  cout << endl << "best k value is " << kparam << " with a time of " <<  k_array[k_min] << " ms."<<endl;
+
+  delete[] array;
+  delete[] origarray;
+  delete[] k_array;
+  array=nullptr;
+  origarray=nullptr;
+  k_array=nullptr;
+}
+void extra_credit_knuth_optimized_qsort_lomuto(const int n) {
+  cout << endl << "Knuth optimized qsort lomuto " << endl << endl;
+  
+  //Knuth Optimized quicksort
+  
+  // initializes variables
+  clock_t t1;
+  clock_t t2;
+  double t;
+  int size = n; // sets the size of the array
+  int* array = new int[size];
+  int* origarray = new int[size];
+  constexpr int k_size = 21;
+  float* k_array = new float[k_size];
+
+  // populate the array
+  for(int i = 0; i < size; i++)
+      array[i] = (i + 1)*(i + 1);
+  fischeryates_shuffle(array,0, size-1); // randomize the array
+  // save the shuffled array
+  for(int i = 0; i < size; i++){
+      origarray[i] = array[i];
+  }
+
+  // run through different values of k
+  int kparam;
+  for(int k = 0; k < k_size; k++){
+     kparam = (k > 0)?((k/10)*1000+k*100):((k/10)*1000+k*100+4);
+      // reset back to the shuffled array for each value of k
+      for(int i = 0; i < size; i++){
+          array[i] = origarray[i];
+      }
+      t1 = clock();
+      quicksort_lomuto(array, 0, size -1, kparam);
+      t2 = clock();
+      std::cout << "n=" << size << "   time=" << ((double) (t2 - t1)) / CLOCKS_PER_SEC<< "   k=" << kparam << endl;
+      // printit(array, size);
+      k_array[k] = ((double) (t2 - t1)) / CLOCKS_PER_SEC; // save the values of runtime for each k
+  }
+
+  int k_min = goldensearch(k_array, k_size);
+  kparam = (k_min > 0)?((k_min/10)*1000+k_min*100):((k_min/10)*1000+k_min*100+4);
+  cout << endl << "best k value is " << kparam << " with a time of " <<  k_array[k_min] << " ms."<<endl;
+
+  delete[] array;
+  delete[] origarray;
+  delete[] k_array;
+  array=nullptr;
+  origarray=nullptr;
+  k_array=nullptr;
 }
